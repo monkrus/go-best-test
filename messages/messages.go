c5f956410d3783eeff1b2775c164632b576fc2b0
Sergei Stadnik <sergeisqa@gmail.com>
2020-04-13 22:15:58 -0700

    upd

--- messages/messages.go
+++ messages/messages.go
@@ -4,10 +4,9 @@ import "fmt"
 
 // Greet is ...
 func Greet(name string) string {
-
-	return fmt.Sprintf("Hello, %v!\n , name")
+	return fmt.Sprintf("Hello, %v!\n", name)
 }
 
 func depart(name string) string {
-	return fmt.Sprintf("Bye, %v!\n , name")
+	return fmt.Sprintf("Bye, %v!\n", name)
 }
--- messages/messages_test.go
+++ messages/messages_test.go
@@ -7,14 +7,30 @@ func TestGreet(t *testing.T) {
 	expect := "Hello, Gopher!\n"
 
 	if got != expect {
-		t.Errorf("Did not get expected results. Wanted %q, got: %q\n", expected, got)
+		t.Errorf("Did not get expected results. Wanted %q, got: %q\n", expect, got)
 	}
 }
+func TestGreetTableDriven(t *testing.T) {
+	scenarios := []struct {
+		input  string
+		expect string
+	}{
+		{input: "Gopher", expect: "Hello, Gopher!\n"},
+		{input: " ", expect: "Hello,  !\n"},
+	}
+	for _, s := range scenarios {
+		got := Greet(s.input)
+		if got != s.expect {
+			t.Errorf("Did not get expected results for input '%v'. Expected %q, got: %q", s.input, got, s.expect)
+		}
+	}
+}
+
 func TestDepart(t *testing.T) {
-	got := Greet("Gopher")
-	expect := "Bye, gopher!\n"
+	got := depart("Gopher")
+	expect := "Bye, Gopher!\n"
 
 	if got != expect {
-		t.Errorf("Did not get expected results. Wanted %q, got: %q\n", expected, got)
+		t.Errorf("Did not get expected results. Wanted %q, got: %q\n", expect, got)
 	}
 }
